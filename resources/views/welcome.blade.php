c5e4e1d3fd453ded44b55402cfea215dba6f6e1c
unknown <edegraftamaning85@gmail.com>
2022-08-18 14:58:22 +0000

    Latest update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,16 @@
     {{--        image and heading--}}
     <div class="row">
         <div class="col p-0">
-            <img class="" width="100%" height="100%" src="/storage/mountani top.jpg" alt="">
+            <div class="" style="background-image: url('/storage/girls6.jpg'); background-attachment:fixed; width:auto; height:85vh; background-size:cover; background-repeat:no-repeat">
+                <div class="" style="text-align: center; padding-top: 35%; height: 100px; line-height: 100px; text-align: center; color:white">
+                <h1 style="text-transform: uppercase; font-weight:bolder">Transforming lives, one girl at a time</h1>
+                <h4 style="font-weight:bolder">Dignity!, Protection! Education! Empowerment</h4>
+            </div>
+            </div>
+            <!-- <img class="" width="100%" height="100%" src="/storage/girls6.jpg" alt=""> -->
         </div>
     </div>
-    {{--        <h1 style="text-align: center;">Empowering Women & Girls Worldwide</h1>--}}
+
     {{--      color cards  --}}
     {{--    various cards--}}
     <div class="row" style="color: white;">
@@ -49,8 +55,8 @@
                         <hr>
                         <div class="row" style="padding:0px 0px 0px 15px">
                             <div class="col p-0">
-                                <p class="">Made from soft absorbent organic  fabrics carefully, 
-                                    engineered for high absorption and preventing leakages.<br>   
+                                <p class="">Made from soft absorbent organic fabrics carefully,
+                                    engineered for high absorption and preventing leakages.<br>
                             </div>
                         </div>
                     </div>
@@ -66,7 +72,7 @@
                         <hr>
                         <div class="row" style="padding:0px 0px 0px 15px">
                             <div class="col p-0">
-                                <p class="">Made of organic fabrics that is skin friendly and safe. 
+                                <p class="">Made of organic fabrics that is skin friendly and safe.
                                     Suitable for daily use. Place and order here</p>
                             </div>
 
@@ -79,7 +85,7 @@
             </div>
             <div class="col-sm">
                 <div class="card card-right" id="card-right">
-                    <img class="card-img-top" src="/storage/mountani top.jpg" alt="Card image">
+                    <img class="card-img-top" src="/storage/girl.jpg" alt="Card image">
                     <div class="card-body">
                         <a href="{{route('news_more')}}" style="text-decoration: none;">
                             <h4 class="card-title">Wet bags/Pouch</h4>
@@ -87,7 +93,8 @@
                         <hr>
                         <div class="row" style="padding:0px 0px 0px 15px">
                             <div class="col p-0">
-                                <p class="">Water proof beautiful bags designed to carry few single reusable pads for storage and transport. </p>
+                                <p class="">Water proof beautiful bags designed to carry few single reusable pads for
+                                    storage and transport. </p>
                             </div>
 
                         </div>
@@ -106,160 +113,155 @@
                 <div class="col-md-4"></div>
                 <div class="col-md-3"></div>
                 <div class="col-md-5">
-                    <div class="card rcard">
-                        <div class="believe-head" style="background-color: #213e8c;">
-                        <p>We believe in menstruation as an avenue for empowerment. Therefore we:</p></div>                    
-                    </div>
+                    <ul style="padding-top: 10%; text-align:right; font-size:20px; list-style:none">
 
-                    <div class="card rcard" style="color: white; text-align:left; padding:10px 10px 10px 20px; background-color:#c84869; width:90%; margin:10px 0px 10px 50px">
-                                Create affordable and environmentally friendly sanitary materials for girls and women to manage their
-                                 menstruation
-                        </div>
+                        <strong>We believe in menstruation as an avenue for empowerment. Therefore we:</strong>
+                        <hr>
+                        <li> <em> Create affordable and environmentally friendly sanitary materials for girls and women
+                                to manage their
+                                menstruation.</em></li>
+                        <hr>
 
-                        <div class="card rcard" style="color: white; text-align:left; padding:10px 10px 10px 20px; background-color:#2a70de; width:90%; margin:10px 50px 10px 0px">
-                        Break the stigma and barriers associated with menstruation for women and girls
-                        </div>
-
-                        <div class="card rcard" style="color: white; text-align:left; padding:10px 10px 10px 20px; background-color:#2a70de; width:90%; margin:10px 50px 10px 0px">
-                        Advocate for a supportive environment for menstruating females
-                        </div>
+                        <li> <em> Break the stigma and barriers associated with menstruation for women and girls. </em>
+                        </li>
+                        <hr>
+                        <li> <em> Advocate for a supportive environment for menstruating females</em> </li>
+                    </ul>
                 </div>
             </div>
-         <div class="col" style="text-align: right;">
-            <p>We believe in menstruation as an avenue for empowerment. Therefore we:<br></p><hr>
-            <h5> &#9792 Create affordable and environmentally friendly sanitary materials for girls and women to manage their
-                menstruation.</h5>
-                <hr>                
-            <h5>  &#9792 Break the stigma and barriers associated with menstruation for women and girls.</h5><hr>
-            <h5> &#9792 Advocate for a supportive environment for menstruating females</h5>
-        </div>
-    </div>
-</div>
+            <div class="col" style="text-align: right;">
 
-{{--        upcomming events--}}
-<h1 class="text-center pt-5 pb-3">Upcoming Events</h1>
 
-{{--        events--}}
-<div class="container p-5">
-    {{--        one event list--}}
-    <div class="row">
-        {{--            first column of row --}}
-        <div class="col-sm-3 d-inline-flex">
-            <h1 class="pr-1">23</h1>
-            <div class="d-inline-block">
-                <h6 id="d_1">sat</h6>
-                <h6 id="d_2">sep</h6>
             </div>
         </div>
-        {{--            second column of row--}}
-        <div class="col-sm-6">
-            <p>10k Against Breast Cancer / San Francisco</p>
-        </div>
-        {{--            third column of row --}}
-        <div class="col-sm-3">
-            {{--    donate button --}}
-            <a href="#" id="d" class="pb-4 pt-3 d-flex justify-content-center align-items-center">
-                <div class="donate-btn pt-2 pb-2">
-                    Register Now
-                </div>
-            </a>
-        </div>
     </div>
-    <hr>
-    {{--        one event list--}}
-    <div class="row">
-        {{--            first column of row --}}
-        <div class="col-sm-3 d-inline-flex">
-            <h1 class="pr-1">23</h1>
-            <div class="d-inline-block">
-                <h6 id="d_1">sat</h6>
-                <h6 id="d_2">sep</h6>
-            </div>
-        </div>
-        {{--            second column of row--}}
-        <div class="col-sm-6">
-            <p>10k Against Breast Cancer / San Francisco</p>
-        </div>
-        {{--            third column of row --}}
-        <div class="col-sm-3">
-            {{--    donate button --}}
-            <a href="#" id="d" class="pb-4 pt-3 d-flex justify-content-center align-items-center">
-                <div class="donate-btn pt-2 pb-2">
-                    Register Now
-                </div>
-            </a>
-        </div>
-    </div>
-    <hr>
-    {{--        one event list--}}
-    <div class="row">
-        {{--            first column of row --}}
-        <div class="col-sm-3 d-inline-flex">
-            <h1 class="pr-1">23</h1>
-            <div class="d-inline-block">
-                <h6 id="d_1">sat</h6>
-                <h6 id="d_2">sep</h6>
-            </div>
-        </div>
-        {{--            second column of row--}}
-        <div class="col-sm-6">
-            <p>10k Against Breast Cancer / San Francisco</p>
-        </div>
-        {{--            third column of row --}}
-        <div class="col-sm-3">
-            {{--    donate button --}}
-            <a href="#" id="d" class="pb-4 pt-3 d-flex justify-content-center align-items-center">
-                <div class="donate-btn pt-2 pb-2">
-                    Register Now
-                </div>
-            </a>
-        </div>
-    </div>
-</div>
 
-{{--join us quote with image--}}
-<div class="row">
-    <div class="col-sm-5">
-        {{--        join us paragraph--}}
-        <div class="row color-1 text-center pt-5 pb-5" style="color: white;">
-            <div class="col">
-                <h1 style="font-weight: bold;">Join Us</h1>
-                <h5>Empower Our Women And Girls</h5>
+    {{--        upcomming events--}}
+    <h1 class="text-center pt-5 pb-3">Upcoming Events</h1>
+
+    {{--        events--}}
+    <div class="container p-5">
+        {{--        one event list--}}
+        <div class="row">
+            {{--            first column of row --}}
+            <div class="col-sm-3 d-inline-flex">
+                <h1 class="pr-1">23</h1>
+                <div class="d-inline-block">
+                    <h6 id="d_1">sat</h6>
+                    <h6 id="d_2">sep</h6>
+                </div>
+            </div>
+            {{--            second column of row--}}
+            <div class="col-sm-6">
+                <p>10k Against Breast Cancer / San Francisco</p>
+            </div>
+            {{--            third column of row --}}
+            <div class="col-sm-3">
                 {{--    donate button --}}
                 <a href="#" id="d" class="pb-4 pt-3 d-flex justify-content-center align-items-center">
-                    <div class="donate-btn">
-                        Donate
+                    <div class="donate-btn pt-2 pb-2">
+                        Register Now
+                    </div>
+                </a>
+            </div>
+        </div>
+        <hr>
+        {{--        one event list--}}
+        <div class="row">
+            {{--            first column of row --}}
+            <div class="col-sm-3 d-inline-flex">
+                <h1 class="pr-1">23</h1>
+                <div class="d-inline-block">
+                    <h6 id="d_1">sat</h6>
+                    <h6 id="d_2">sep</h6>
+                </div>
+            </div>
+            {{--            second column of row--}}
+            <div class="col-sm-6">
+                <p>10k Against Breast Cancer / San Francisco</p>
+            </div>
+            {{--            third column of row --}}
+            <div class="col-sm-3">
+                {{--    donate button --}}
+                <a href="#" id="d" class="pb-4 pt-3 d-flex justify-content-center align-items-center">
+                    <div class="donate-btn pt-2 pb-2">
+                        Register Now
+                    </div>
+                </a>
+            </div>
+        </div>
+        <hr>
+        {{--        one event list--}}
+        <div class="row">
+            {{--            first column of row --}}
+            <div class="col-sm-3 d-inline-flex">
+                <h1 class="pr-1">23</h1>
+                <div class="d-inline-block">
+                    <h6 id="d_1">sat</h6>
+                    <h6 id="d_2">sep</h6>
+                </div>
+            </div>
+            {{--            second column of row--}}
+            <div class="col-sm-6">
+                <p>10k Against Breast Cancer / San Francisco</p>
+            </div>
+            {{--            third column of row --}}
+            <div class="col-sm-3">
+                {{--    donate button --}}
+                <a href="#" id="d" class="pb-4 pt-3 d-flex justify-content-center align-items-center">
+                    <div class="donate-btn pt-2 pb-2">
+                        Register Now
                     </div>
                 </a>
             </div>
         </div>
     </div>
-    <div class="col-sm-7">
-        {{--        image of togetherness--}}
-        <div class="row">
-            <div class="col p-0">
-                <img class="" src="/storage/a.png" width="100%" height="100%" alt="">
+
+    {{--join us quote with image--}}
+    <div class="row">
+        <div class="col-sm-5">
+            {{--        join us paragraph--}}
+            <div class="row color-1 text-center pt-5 pb-5" style="color: white;">
+                <div class="col">
+                    <h1 style="font-weight: bold;">Join Us</h1>
+                    <h5>Empower Our Women And Girls</h5>
+                    {{--    donate button --}}
+                    <a href="#" id="d" class="pb-4 pt-3 d-flex justify-content-center align-items-center">
+                        <div class="donate-btn">
+                            Donate
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-7">
+            {{--        image of togetherness--}}
+            <div class="row">
+                <div class="col p-0">
+                    <img class="" src="/storage/a.png" width="100%" height="100%" alt="">
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
 </div>
 @endsection
 
 <style>
-    .believe{
-        background-image: url('storage/mountani top.jpg');
-        background-repeat: no-repeat;
-        background-size: cover;
-        height: 70vh;
-    }
+.believe {
+    background-image: url('storage/girl.jpg');
+    background-attachment: fixed;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 70vh;
+}
 
-    .believe-head{
-        padding: 10px;
-        font-weight: bold;
-    }
+.believe-head {
+    padding: 10px;
+    font-weight: bold;
+}
+
 @keyframes transitionIn {
     from {
         opacity: 0;
@@ -304,17 +306,17 @@ window.onscroll = function() {
 }
 
 function scrollFunction() {
-    if (pageYOffset > 600 && pageYOffset < 1000) {
+    if (pageYOffset > 700 && pageYOffset < 1000) {
         document.getElementById("card-left").style.animation = "transitionIn 1.5s";
         document.getElementById("card-left").style.visibility = "visible";
     }
 
-    if (pageYOffset > 650 && pageYOffset < 1000) {
+    if (pageYOffset > 700 && pageYOffset < 1000) {
         document.getElementById("card-middle").style.animation = "transitionIn 2.5s";
         document.getElementById("card-middle").style.visibility = "visible";
     }
 
-    if (pageYOffset > 650 && pageYOffset < 1000) {
+    if (pageYOffset > 700 && pageYOffset < 1000) {
         document.getElementById("card-right").style.animation = "transitionIn 3.5s";
         document.getElementById("card-right").style.visibility = "visible";
     }
